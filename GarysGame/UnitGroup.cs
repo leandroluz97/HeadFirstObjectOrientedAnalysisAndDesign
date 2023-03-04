3498dc828da639a99eb4fcf734f6c9b67fab9a7d
leandroluz97 <leandrodaluz97@gmail.com>
2023-03-04 14:08:01 +0000

    added UnitGroup service class

---
+++ GarysGame/UnitGroup.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GarysGame
+{
+    public class UnitGroup
+    {
+        public string Name { get; private set; }
+        private List<Unit> _units { get; set; }
+        public  List<Unit>  Units { get { return _units; } }
+
+        public UnitGroup(string name)
+        {
+            Name = name;
+        }
+
+        public void AddUnit(Unit unit)
+        {
+            if(_units == null)
+            {
+                _units = new List<Unit>();
+            }
+            _units.Add(unit);
+        }
+        public void RemoveUnit(int? id)
+        {
+            if(id == null)
+            {
+                throw new ArgumentNullException(nameof(id));
+            }
+            if(_units == null)
+            {
+                throw new Exception("Can't delete unit from empty group.");
+            }
+
+            Unit? unit = _units.FirstOrDefault(u => u.Id == id);
+
+            if (unit == null)
+            {
+                throw new Exception();
+            }
+            _units.Where(unit => unit.Id != id);
+        }
+    }
+}
