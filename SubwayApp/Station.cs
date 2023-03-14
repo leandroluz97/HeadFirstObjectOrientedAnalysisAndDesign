e6150d5baeee992ba09d2579e661641452453a36
leandroluz97 <leandrodaluz97@gmail.com>
2023-03-14 22:01:29 +0000

    added subwayApp project

---
+++ SubwayApp/Station.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SubwayApp
+{
+    public class Station
+    {
+        private string _name;
+        public string Name { get { return _name; } }
+
+        public Station(string name)
+        {
+            _name = name;
+        }
+
+        public bool Equals(Object obj)
+        {
+            return true;
+        }
+
+    }
+}
