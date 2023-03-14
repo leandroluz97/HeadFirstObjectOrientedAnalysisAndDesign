2b7d5bea49284c274fbb44c3fa9a8501fd576005
leandroluz97 <leandrodaluz97@gmail.com>
2023-03-14 22:26:49 +0000

    added connection class

---
+++ SubwayApp/Connection.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SubwayApp
+{
+    public class Connection
+    {
+        public Station StationOne { get; set; }
+        public Station StationTwo { get; set; }
+        public string LineName { get; set; }
+
+        public Connection(Station stationOne, Station stationTwo, string lineName)
+        {
+            StationOne = stationOne;
+            StationTwo = stationTwo;
+            LineName = lineName;
+        }
+        
+    }
+}
