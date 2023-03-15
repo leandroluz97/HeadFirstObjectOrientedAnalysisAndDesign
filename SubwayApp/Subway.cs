f4103edd76f040a085f506837d8a90990665de93
leandroluz97 <leandrodaluz97@gmail.com>
2023-03-15 00:40:12 +0000

    added subwayLoader class

---
+++ SubwayApp/Subway.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SubwayApp
+{
+    public class Subway
+    {
+        public List<Station> Stations { get; set; }
+        public List<Connection> Connections { get; set; }
+
+        public Subway()
+        {
+            Stations = new List<Station>();
+            Connections = new List<Connection>();
+        }
+
+        public void AddStation(string stationName)
+        {
+            if(HasStation(stationName))
+            {
+                Station station = new Station(stationName);
+                Stations.Add(station);
+            }
+            
+        }
+
+        public  bool HasStation(string stationName)
+        {
+            return Stations.Contains(new Station(stationName));
+        }
+        public void addConnection(string stationOneName, string stationTwoName, string lineName)
+        {
+            if((HasStation(stationOneName) && HasStation(stationTwoName)))
+            {
+                Station stationOne = new Station(stationOneName);
+                Station stationTwo = new Station(stationTwoName);  
+                Connection connection  = new Connection(stationOne, stationTwo, lineName);
+                Connections.Add(connection);
+            }
+            else
+            {
+                throw new Exception($"Either {nameof(stationOneName)} or {nameof(stationTwoName)} don't exist!");
+            }
+        }
+
+    }
+}
---
+++ SubwayApp/SubwayLoader.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SubwayApp
+{
+    public class SubwayLoader
+    {
+        public Subway Subway { get; set; }
+        public SubwayLoader()
+        {
+            Subway = new Subway();
+        }
+
+        //public Subway LoadFromFile(File subwayFile) 
+        //{
+
+        //}
+
+        //public void SubwayLoader(Subway subway)
+        //{
+            
+        //}
+
+     }
+}
