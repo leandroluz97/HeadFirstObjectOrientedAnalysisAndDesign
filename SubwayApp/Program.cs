0e9d85caa268f9e07efa7029026517e0e596b61c
leandroluz97 <leandrodaluz97@gmail.com>
2023-03-22 01:47:49 +0000

    implemented PrintDirections printer method

--- SubwayApp/Program.cs
+++ SubwayApp/Program.cs
@@ -31,6 +31,39 @@ public class HelloWorld
             Console.WriteLine($"Station name: {station.Name}");
         }
 
+        var printer =  new SubwayPrinter();
+      
+        Station station1 = new Station("XHTML Expressway");
+        Station station2 = new Station("Infinite Circle");
+        Station station3 = new Station("Infinite Circle");
+        Station station4 = new Station("Head First Theater");
+        Station station5 = new Station("Head First Theater");
+        Station station6 = new Station("DRY drive");
+        Station station7 = new Station("DRY drive");
+        Station station8 = new Station("Web Design Way");
+        Station station9 = new Station("Web Design Way");
+        Station station10 = new Station("EJB Estates");
+        Station station11 = new Station("EJB Estates");
+        Station station12 = new Station("JSP Junction");
+        
+        var connections = new List<Connection>()
+        {
+            new Connection(station1, station2, "Rumbaugh Line"),
+            new Connection(station2, station3, "Rumbaugh Line"),
+            new Connection(station3, station4, "Meyer Line"),
+            new Connection(station4, station5, "Meyer Line"),
+            new Connection(station5, station6, "Wirfs-Brock"),
+            new Connection(station6, station7, "Wirfs-Brock"),
+            new Connection(station7, station8, "Wirfs-Brock"),
+            new Connection(station8, station9, "Liskov Line"),
+            new Connection(station9, station10, "Liskov Line"),
+            new Connection(station10, station11, "Liskov Line"),
+            new Connection(station11, station12, "Liskov Line"),
+    };
+
+        printer.PrintDirections(connections);
+
+
 
 
     }
--- SubwayApp/SubwayPrinter.cs
+++ SubwayApp/SubwayPrinter.cs
@@ -14,16 +14,22 @@ namespace SubwayApp
             {
                 if(i == 0)
                 {
-                    Console.WriteLine($"Get on the {connections[0].StationOne.Name} heading towards {connections[0].StationTwo.Name}");
+                    Console.WriteLine($"Get on the {connections[i].StationOne.Name} heading towards {connections[i].StationTwo.Name}");
                     continue;
                 }
                 if(connections[i].LineName == connections[i - 1].LineName)
                 {
                     Console.WriteLine($"Continue {connections[i - 1].StationTwo.Name}...");
                 }
+                if((i + 1) >= connections.Count - 1)
+                {
+                    Console.WriteLine($"Get out and enjoy {connections[i - 1].StationTwo.Name} ");
+                    break;
+                }
                 if(connections[i].LineName != connections[i + 1].LineName)
                 {
                     Console.WriteLine($"When you get {connections[i].StationTwo.Name} get off {connections[i].LineName}");
+                    Console.WriteLine($"Switch over to the {connections[i].StationTwo.Name} heading towards {connections[i].LineName}");
                 }
             }
         }
